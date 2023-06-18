7d1c964f201f859eb4d13c6dfcd743fc0c3169f9
Denis Kuznetsov <kyznecov995@icloud.com>
2023-06-19 02:01:30 +0400

    add gh link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,12 @@ const Header = () => {
 					</a>
 					<ul id='nav-mobile' className='right hide-on-med-and-down'>
 						<li>
-							<a href='collapsible.html'>GitHub</a>
+							<a
+								href='https://github.com/KuznecovDenis/react-vitrina'
+								target='_blank'
+								rel='noreferrer'>
+								GitHub
+							</a>
 						</li>
 					</ul>
 				</div>
